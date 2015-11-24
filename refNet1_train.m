dd8e20b862b9578350c8650a6dc320dd5b9c7989
Vincent Kee <vkee@mit.edu>
2015-11-24 10:26:33 -0500

    current state

--- refNet1_train.m
+++ refNet1_train.m
@@ -1,4 +1,4 @@
-function [net, info] = refNet1(varargin)
+function [net, info] = refNet1_train(varargin)
 % Training refNet1 CNN
 
 run(fullfile(fileparts(mfilename('fullpath')), ...
@@ -11,13 +11,17 @@ opts.expDir = fullfile('data', 'refNet1') ;
 opts.train.numEpochs = numel(opts.train.learningRate) ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
 
-opts.dataDir = fullfile('data','cifar') ;
+% Need to update this with directory of data
+opts.dataDir = fullfile('data','images') ;
 opts.imdbPath = fullfile(opts.expDir, 'imdb.mat');
 opts.whitenData = true ;
 opts.contrastNormalization = true ;
 opts.train.batchSize = 100 ;
 opts.train.continue = true ;
+
 opts.train.gpus = [] ; % probably want to use this, ask for what should put here
+% Jake, this is where you put in the GPU stuff
+
 opts.train.learningRate = 0.001 ; % not sure what should have here
 opts.train.expDir = opts.expDir ;
 opts = vl_argparse(opts, varargin) ;
@@ -29,7 +33,7 @@ opts = vl_argparse(opts, varargin) ;
 if exist(opts.imdbPath, 'file')
   imdb = load(opts.imdbPath) ;
 else
-  imdb = getCifarImdb(opts) ;
+  imdb = getChallengeImdb(opts) ;
   mkdir(opts.expDir) ;
   save(opts.imdbPath, '-struct', 'imdb') ;
 end
@@ -55,7 +59,7 @@ labels = imdb.images.labels(1,batch) ;
 if rand > 0.5, im=fliplr(im) ; end
 
 % --------------------------------------------------------------------
-function imdb = getCifarImdb(opts)
+function imdb = getChallengeImdb(opts)
 % --------------------------------------------------------------------
 % Preapre the imdb structure, returns image data with mean image subtracted
 % Need to update with path
@@ -64,12 +68,6 @@ files = [arrayfun(@(n) sprintf('data_batch_%d.mat', n), 1:5, 'UniformOutput', fa
   {'test_batch.mat'}];
 files = cellfun(@(fn) fullfile(unpackPath, fn), files, 'UniformOutput', false);
 file_set = uint8([ones(1, 5), 3]);
-
-if any(cellfun(@(fn) ~exist(fn, 'file'), files))
-  url = 'http://www.cs.toronto.edu/~kriz/cifar-10-matlab.tar.gz' ;
-  fprintf('downloading %s\n', url) ;
-  untar(url, opts.dataDir) ;
-end
 
 data = cell(1, numel(files));
 labels = cell(1, numel(files));
@@ -88,28 +86,29 @@ data = single(cat(4, data{:}));
 dataMean = mean(data(:,:,:,set == 1), 4);
 data = bsxfun(@minus, data, dataMean);
 
-% normalize by image mean and std as suggested in `An Analysis of
-% Single-Layer Networks in Unsupervised Feature Learning` Adam
-% Coates, Honglak Lee, Andrew Y. Ng
-
-if opts.contrastNormalization
-  z = reshape(data,[],60000) ;
-  z = bsxfun(@minus, z, mean(z,1)) ;
-  n = std(z,0,1) ;
-  z = bsxfun(@times, z, mean(n) ./ max(n, 40)) ;
-  data = reshape(z, 32, 32, 3, []) ;
-end
-
-if opts.whitenData
-  z = reshape(data,[],60000) ;
-  W = z(:,set == 1)*z(:,set == 1)'/60000 ;
-  [V,D] = eig(W) ;
-  % the scale is selected to approximately preserve the norm of W
-  d2 = diag(D) ;
-  en = sqrt(mean(d2)) ;
-  z = V*diag(en./max(sqrt(d2), 10))*V'*z ;
-  data = reshape(z, 32, 32, 3, []) ;
-end
+% Add in these later to improve
+% % normalize by image mean and std as suggested in `An Analysis of
+% % Single-Layer Networks in Unsupervised Feature Learning` Adam
+% % Coates, Honglak Lee, Andrew Y. Ng
+% 
+% if opts.contrastNormalization
+%   z = reshape(data,[],60000) ;
+%   z = bsxfun(@minus, z, mean(z,1)) ;
+%   n = std(z,0,1) ;
+%   z = bsxfun(@times, z, mean(n) ./ max(n, 40)) ;
+%   data = reshape(z, 32, 32, 3, []) ;
+% end
+% 
+% if opts.whitenData
+%   z = reshape(data,[],60000) ;
+%   W = z(:,set == 1)*z(:,set == 1)'/60000 ;
+%   [V,D] = eig(W) ;
+%   % the scale is selected to approximately preserve the norm of W
+%   d2 = diag(D) ;
+%   en = sqrt(mean(d2)) ;
+%   z = V*diag(en./max(sqrt(d2), 10))*V'*z ;
+%   data = reshape(z, 32, 32, 3, []) ;
+% end
 
 clNames = load(fullfile(unpackPath, 'batches.meta.mat'));
 
