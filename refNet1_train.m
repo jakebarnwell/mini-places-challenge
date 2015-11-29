7b93fa704d88722280412eea37fbec29c9c6d966
Jake Barnwell <jb16@mit.edu>
2015-11-29 14:43:04 -0500

    Deleting and renaming shit

--- refNet1_train.m
+++ refNet1_train.m
@@ -1,19 +1,24 @@
-function [net, info] = refNet1_train(varargin)
-% Training refNet1 CNN
+function refNet1_train_imagenet(varargin)
+% REFNET1_TRAIN_IMAGENET  Copies the style of cnn_imagenet
+%   This tries to train the miniplaces competition net
+
+addpath(fullfile('matconvnet','examples'));
+addpath(fullfile('matconvnet','matlab'));
 
 run(fullfile(fileparts(mfilename('fullpath')), ...
   'matconvnet', 'matlab', 'vl_setupnn.m')) ;
 
-opts.dataDir = fullfile('data','ILSVRC2012') ;
-opts.modelType = 'refNet1';
-opts.networkType = 'simplenn';
+opts.dataDir = fullfile('data') ; %Contains our images/objects etc
+opts.modelType = 'refNet1' ;
+opts.networkType = 'simplenn' ;
 opts.batchNormalization = false ;
 opts.weightInitMethod = 'gaussian' ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
 
 sfx = opts.modelType ;
-opts.expDir = fullfile('data', sprintf('places', ...
-                                       sfx, opts.networkType)) ;
+if opts.batchNormalization, sfx = [sfx '-bnorm'] ; end
+opts.expDir = fullfile(opts.dataDir, 'refnet', ...
+    sprintf('refnet-%s-%s', sfx, opts.networkType)) ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
 
 opts.numFetchThreads = 12 ;
@@ -22,7 +27,7 @@ opts.imdbPath = fullfile(opts.expDir, 'imdb.mat');
 opts.train.batchSize = 256 ;
 opts.train.numSubBatches = 1 ;
 opts.train.continue = true ;
-opts.train.gpus = [] ; % jake you'll want to change this
+opts.train.gpus = [1, 2, 3, 4] ;
 opts.train.prefetch = true ;
 opts.train.sync = false ;
 opts.train.cudnn = true ;
@@ -37,163 +42,110 @@ end
 opts.train.numEpochs = numel(opts.train.learningRate) ;
 opts = vl_argparse(opts, varargin) ;
 
+% -------------------------------------------------------------------------
+%                                                   Database initialization
+% -------------------------------------------------------------------------
+
 if exist(opts.imdbPath)
   imdb = load(opts.imdbPath) ;
 else
-  imdb = setup_data('dataDir', opts.dataDir, 'lite', opts.lite) ;
+  % TODO this function
+  imdb = refNet1_imagenet_setup_data('dataDir', opts.dataDir, 'lite', opts.lite) ;
   mkdir(opts.expDir) ;
   save(opts.imdbPath, '-struct', 'imdb') ;
 end
 
+% All imdb.* fields are 1 x n (not n x 1)
+% imdb.classes.name = {className1, className2, ...}
+% imdb.classes.description = {classDescr1, classDescr2, ...}
+% where "className" is like an ID, and "classDescr" is like 'White Tiger'
+% imdb.imageDir is the top level directory of the images, e.g. if we had
+% data/imagenet12/images/{test,train,val} then imdb.imageDir is
+% data/imagenet12/images
+% imdb.images.id is an array of unique IDs of images, e.g. [1, 2, ...]
+% imdb.images.name is a cell array of full path names (relative to
+% imageDir) to each of the images, e.g. {train/classNameA/classAimage1.JPG,
+% ...}
+% imdb.images.set is an array of the set type of the images, where 1 =
+% train, 2 = validate, 3 = test. So it will probably look like this:
+%    [1 1 1 .... 2 2 2 .... 3 3 3 ... ]
+% imdb.images.label is an array, where each element is the index into the
+% classes cell array that tells us to which class the image belongs. e.g.
+% [28 193 930 68 ... ]
 
+% -------------------------------------------------------------------------
+%                                                    Network initialization
+% -------------------------------------------------------------------------
 
+net = sample_refNet_initial('model', opts.modelType, ...
+			    'batchNormalization', opts.batchNormalization, ...
+                        'weightInitMethod', opts.weightInitMethod) ;
+bopts = net.normalization ;
+bopts.numThreads = opts.numFetchThreads ;
 
-
-opts.modelType = 'refNet1_train';
-[opts, varargin] = vl_argparse(opts, varargin) ;
-
-opts.train.learningRate = 0.001 ; % not sure what should have here
-opts.train.weightDecay = 0.001 ; % not sure what should have here
-
-% Need to update with location of the data
-opts.expDir = fullfile('data') ;
-[opts, varargin] = vl_argparse(opts, varargin) ;
-
-opts.train.numEpochs = numel(opts.train.learningRate) ;
-[opts, varargin] = vl_argparse(opts, varargin) ;
-
-opts.dataDir = fullfile(opts.expDir, 'images', 'train');
-opts.imdbPath = fullfile(opts.expDir, 'imdb.mat');
-opts.whitenData = true ;
-opts.contrastNormalization = true ;
-opts.train.batchSize = 100 ;
-opts.train.continue = true ;
-
-opts.train.gpus = [] ; % probably want to use this, ask for what should put here
-% Jake, this is where you put in the GPU stuff
-
-opts.train.expDir = opts.expDir ;
-opts = vl_argparse(opts, varargin) ;
-
-categories = readtable(fullfile(fileparts(mfilename('fullpath')), ...
-  'development_kit', 'data', 'categories.txt'), 'Delimiter',' ');
-
-% --------------------------------------------------------------------
-%                                               Prepare data and model
-% --------------------------------------------------------------------
-
-if exist(opts.imdbPath, 'file')
-  imdb = load(opts.imdbPath) ;
+% compute image statistics (mean, RGB covariances etc)
+imageStatsPath = fullfile(opts.expDir, 'imageStats.mat') ;
+if exist(imageStatsPath)
+  load(imageStatsPath, 'averageImage', 'rgbMean', 'rgbCovariance') ;
 else
-  imdb = getChallengeImdb(opts) ;
-  mkdir(opts.expDir) ;
-  save(opts.imdbPath, '-struct', 'imdb') ;
+  [averageImage, rgbMean, rgbCovariance] = getImageStats(imdb, bopts) ;
+  save(imageStatsPath, 'averageImage', 'rgbMean', 'rgbCovariance') ;
 end
 
-net = sample_refNet_initial();
+% One can use the average RGB value, or use a different average for
+% each pixel
+%net.normalization.averageImage = averageImage ;
+net.normalization.averageImage = rgbMean ;
 
-% --------------------------------------------------------------------
-%                                                                Train
-% --------------------------------------------------------------------
+% -------------------------------------------------------------------------
+%                                               Stochastic gradient descent
+% -------------------------------------------------------------------------
 
-[net, info] = cnn_train(net, imdb, @getBatch, ...
-    opts.train, ...
-    'val', find(imdb.images.set == 3)) ;
+[v,d] = eig(rgbCovariance) ;
+bopts.transformation = 'stretch' ;
+bopts.averageImage = rgbMean ;
+bopts.rgbVariance = 0.1*sqrt(d)*v' ;
+useGpu = numel(opts.train.gpus) > 0 ;
 
-% Saving the trained CNN 
-save sample_train.mat net info
+%  getBatchSimpleNNWrapper, cnn_train stuff
+fn = getBatchSimpleNNWrapper(bopts) ;
+[net,info] = cnn_train(net, imdb, fn, opts.train, 'conserveMemory', true) ;
 
-% --------------------------------------------------------------------
-function [im, labels] = getBatch(imdb, batch)
-% --------------------------------------------------------------------
-im = imdb.images.data(:,:,:,batch) ;
-labels = imdb.images.labels(1,batch) ;
-if rand > 0.5, im=fliplr(im) ; end
+% -------------------------------------------------------------------------
+function fn = getBatchSimpleNNWrapper(opts)
+% -------------------------------------------------------------------------
+fn = @(imdb,batch) getBatchSimpleNN(imdb,batch,opts) ;
+
+% -------------------------------------------------------------------------
+function [im,labels] = getBatchSimpleNN(imdb, batch, opts)
+% -------------------------------------------------------------------------
+images = strcat([imdb.imageDir filesep], imdb.images.name(batch)) ;
+im = refNet1_get_batch_imagenet(images, opts, ...
+                            'prefetch', nargout == 0) ;
+labels = imdb.images.label(batch) ;
+
+% -------------------------------------------------------------------------
+function [averageImage, rgbMean, rgbCovariance] = getImageStats(imdb, opts)
+% -------------------------------------------------------------------------
+train = find(imdb.images.set == 1) ;
+train = train(1: 101: end);
+bs = 256 ;
+fn = getBatchSimpleNNWrapper(opts) ;
+for t=1:bs:numel(train)
+  batch_time = tic ;
+  batch = train(t:min(t+bs-1, numel(train))) ;
+  fprintf('collecting image stats: batch starting with image %d ...\n', batch(1)) ;
+  temp = fn(imdb, batch) ;
+  z = reshape(permute(temp,[3 1 2 4]),3,[]) ;
+  n = size(z,2) ;
+  avg{t} = mean(temp, 4) ;
+  rgbm1{t} = sum(z,2)/n ;
+  rgbm2{t} = z*z'/n ;
+  batch_time = toc(batch_time) ;
+  fprintf(' %.2f s (%.1f images/s)\n', batch_time, numel(batch)/ batch_time) ;
 end
-
-% --------------------------------------------------------------------
-function imdb = getChallengeImdb(opts)
-% --------------------------------------------------------------------
-% Prepare the imdb structure, returns image data with mean image subtracted
-% Need to update with path
-
-img_paths = getAllFiles(opts.dataDir);
-% We can do this same thing with the xml data stuff later
-num_imgs = length(img_paths);
-
-data = cell(1, num_imgs);
-labels = cell(1, num_imgs);
-sets = cell(1, num_imgs);
-
-for i = 1:numel(num_imgs)
-  data{i} = imread(char(img_paths(i)));
-%   data{i} = permute(reshape(fd.data',32,32,3,[]),[2 1 3 4]) ;
-%   labels{fi} = fd.labels' + 1; % Index from 1
-
-  sets{fi} = 1; % for the test set this should be set to 3?
-end
-% set needs to eventually be (I think) a 1 x 110k matrix
-% where the first 100k are 1's (meaning that we have 100k
-% training images) and the last 10 are 3's or 2's
-% (meaning we have 10k validation/test images)
-set = cat(2, sets{:});
-% data = single(cat(4, data{:}));
-
-% % remove mean in any case
-% dataMean = mean(data(:,:,:,set == 1), 4);
-% data = bsxfun(@minus, data, dataMean);
-
-% Add in these later to improve
-% % normalize by image mean and std as suggested in `An Analysis of
-% % Single-Layer Networks in Unsupervised Feature Learning` Adam
-% % Coates, Honglak Lee, Andrew Y. Ng
-% 
-% if opts.contrastNormalization
-%   z = reshape(data,[],60000) ;
-%   z = bsxfun(@minus, z, mean(z,1)) ;
-%   n = std(z,0,1) ;
-%   z = bsxfun(@times, z, mean(n) ./ max(n, 40)) ;
-%   data = reshape(z, 32, 32, 3, []) ;
-% end
-% 
-% if opts.whitenData
-%   z = reshape(data,[],60000) ;
-%   W = z(:,set == 1)*z(:,set == 1)'/60000 ;
-%   [V,D] = eig(W) ;
-%   % the scale is selected to approximately preserve the norm of W
-%   d2 = diag(D) ;
-%   en = sqrt(mean(d2)) ;
-%   z = V*diag(en./max(sqrt(d2), 10))*V'*z ;
-%   data = reshape(z, 32, 32, 3, []) ;
-% end
-
-% clNames = load(fullfile(unpackPath, 'batches.meta.mat'));
-
-imdb.images.data = data ;
-% imdb.images.labels = single(cat(2, labels{:})) ;
-imdb.images.set = set;
-imdb.meta.sets = {'train', 'val', 'test'} ;
-% imdb.meta.classes = clNames.label_names;
-
-end
-
-% Searches recursively through all subdirectories of a given directory, 
-% collecting a list of all file paths it finds
-function fileList = getAllFiles(dirName)
-
-  dirData = dir(dirName);      %# Get the data for the current directory
-  dirIndex = [dirData.isdir];  %# Find the index for directories
-  fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
-  if ~isempty(fileList)
-    fileList = cellfun(@(x) fullfile(dirName,x),...  %# Prepend path to files
-                       fileList,'UniformOutput',false);
-  end
-  subDirs = {dirData(dirIndex).name};  %# Get a list of the subdirectories
-  validIndex = ~ismember(subDirs,{'.','..'});  %# Find index of subdirectories
-                                               %#   that are not '.' or '..'
-  for iDir = find(validIndex)                  %# Loop over valid subdirectories
-    nextDir = fullfile(dirName,subDirs{iDir});    %# Get the subdirectory path
-    fileList = [fileList; getAllFiles(nextDir)];  %# Recursively call getAllFiles
-  end
-end
-end
+averageImage = mean(cat(4,avg{:}),4) ;
+rgbm1 = mean(cat(2,rgbm1{:}),2) ;
+rgbm2 = mean(cat(3,rgbm2{:}),3) ;
+rgbMean = rgbm1 ;
+rgbCovariance = rgbm2 - rgbm1*rgbm1' ;
