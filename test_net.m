195416f67bd234bee794016695beb6fcef58c531
Vincent Kee <vkee@mit.edu>
2015-11-29 14:48:43 -0500

    cleaned up test net code

--- test_net.m
+++ test_net.m
@@ -6,22 +6,29 @@ run(fullfile(fileparts(mfilename('fullpath')), ...
 
 % load pre-trained model
 load('categoryIDX.mat');
+
+% Trained CNN - CHANGE THIS TO EVALUATE DIFFERENT NEURAL NETS
 path_model = 'net-epoch-60.mat';
 load([path_model]) ;
 
-% Run for all images in the valid set
-files = dir('data/images/val/*.jpg');
-num_files = length(files);
+% WHETHER RUNNING ON VAL OR TEST SET, CHANGE THIS TO CHANGE WHAT RUNNING ON
+run_on_val_set = true;
 
-fileID = fopen('val_results.txt','w');
-formatSpec = '%s %d %d %d %d %d\n';
+if run_on_val_set
+    % Run for all images in the valid set
+    files = dir('data/images/val/*.jpg');
+    num_files = length(files);
 
-% % Run for all images in the test set
-% files = dir('data/images/test/*.jpg');
-% fileID = fopen('test_results.txt','w');
-% 
-% fileID = fopen('test_results.txt','w');
-% formatSpec = '%s %d %d %d %d %d\n';
+    fileID = fopen('val_results.txt','w');
+    formatSpec = '%s %d %d %d %d %d\n';
+else
+    % Run for all images in the test set
+    files = dir('data/images/test/*.jpg');
+    fileID = fopen('test_results.txt','w');
+    
+    fileID = fopen('test_results.txt','w');
+    formatSpec = '%s %d %d %d %d %d\n';
+end
 
 % change the last layer of CNN from softmaxloss to softmax
 net.layers{1,end}.type = 'softmax';
@@ -42,23 +49,9 @@ for file = files'
     scores = squeeze(gather(res(end).x)) ;
     [score_sort, idx_sort] = sort(scores,'descend') ;
     
-    %     line_data = cell(1, 6);
-    %     line_data(1) = java.lang.String(line_data.name);
-    %
-    %     for i=1:5
-    %     %     write as a line to a text file
-    %         line_data(i+1) = idx_sort(i);
-    % %         disp(sprintf('%s (%d), score %.3f', categoryIDX{idx_sort(i),1}, idx_sort(i), score_sort(i)));
-    %     end
-    
-    %     line_data = {};
-    
     % Write the data to the text file
     fprintf(fileID,formatSpec,strcat('val/', file.name), idx_sort(1), ...
         idx_sort(2), idx_sort(3), idx_sort(4), idx_sort(5));
-%     
-%     fprintf(fileID,formatSpec,strcat('test/', file.name), idx_sort(1), ...
-%         idx_sort(2), idx_sort(3), idx_sort(4), idx_sort(5));
 end
 
 fclose(fileID);
