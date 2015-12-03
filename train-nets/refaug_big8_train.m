20fa1b4d14921b96b0eaec68e299ae771a2bdcf1
Ubuntu <ubuntu@ip-172-31-9-157.ec2.internal>
2015-12-03 17:20:17 +0000

    Add big8 aug

--- train-nets/refaug_big8_train.m
+++ train-nets/refaug_big8_train.m
@@ -13,13 +13,13 @@ run(fullfile(fileparts(mfilename('fullpath')), ...
 opts.dataDir = fullfile('..','data') ;
 opts.modelType = 'refNet1' ;
 opts.networkType = 'simplenn' ;
-opts.batchNormalization = true ;
+opts.batchNormalization = false ;
 opts.weightInitMethod = 'gaussian' ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
 
 sfx = opts.modelType ;
 if opts.batchNormalization, sfx = [sfx '-bnorm'] ; end
-opts.expDir = fullfile(opts.dataDir, 'refnetaug8', ...
+opts.expDir = fullfile(opts.dataDir, 'refnetaug8-big', ...
     sprintf('refnet-%s-%s', sfx, opts.networkType)) ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
 
