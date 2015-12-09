aa10ec6c564a8432e9c6b241edb2ddff09bd1d34
Jake Barnwell <jb16@mit.edu>
2015-12-09 09:48:46 -0500

    add final scores for jakeneet8flip refnet3

--- development_kit/evaluation/demo_eval_cls.m
+++ development_kit/evaluation/demo_eval_cls.m
@@ -4,7 +4,7 @@
 
 fprintf('MINI PLACES SCENE CLASSIFICATION CHALLENGE\n');
 
-pred_file='avg_results.txt';
+pred_file='../../nets/jakenet-aug8flip-refnet3/val-predictions-20.txt';
 ground_truth_file='../data/val.txt';
 num_predictions_per_image=5;
 
