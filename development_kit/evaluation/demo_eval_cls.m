5dd38293b6c21b455f314a242acacd98fc708716
Vincent Kee <vkee@mit.edu>
2015-12-12 01:18:20 -0500

    more scores

--- development_kit/evaluation/demo_eval_cls.m
+++ development_kit/evaluation/demo_eval_cls.m
@@ -4,7 +4,7 @@
 
 fprintf('MINI PLACES SCENE CLASSIFICATION CHALLENGE\n');
 
-pred_file='../../model_averaging_results/jakenet_jamar7_jakenet-aug4flip_jakenet-aug8flip-refnet3_alexnet1_jakenet-flipNoise/val.txt'; %nets/jakenet-flipNoise/val-predictions-21.txt
+pred_file='../../nets/refnet1-b128/val-predictions-20.txt'; %nets/jakenet-flipNoise/val-predictions-21.txt
 ground_truth_file='../data/val.txt';
 num_predictions_per_image=5;
 
